fa6ba6c13317aba44d1c80ef8fbe7d82f81e2495
seeta11 <seeta141722@gmail.com>
2021-12-08 03:57:54 +0300

    login logic & move from LoginActivity.kt to MainActivity.kt

--- app/src/main/java/com/example/mystory/LoginActivity.kt
+++ app/src/main/java/com/example/mystory/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mystory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,49 +9,66 @@ import android.widget.EditText
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
-    private var editTextUsername:EditText?=null
-    private var editTextPassword:EditText?=null
-    private var buttonLogin:Button?=null
-    private var checkboxView:CheckBox?=null
+    private var editTextUsername: EditText? = null
+    private var editTextPassword: EditText? = null
+    private var buttonLogin: Button? = null
+    private var checkboxView: CheckBox? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         connectViews()
         login()
+        //checkFields()
     }
-    private fun connectViews(){
+
+    private fun connectViews() {
         editTextUsername = findViewById(R.id.etUsername)
         editTextPassword = findViewById(R.id.etPassword)
         buttonLogin = findViewById(R.id.btLogin)
         checkboxView = findViewById(R.id.checkbox)
     }
-    private fun login(){
-        val arr:ArrayList<User> = ArrayList()
-        arr.add(User("Seeta123@gmail.com","1234"))
-        arr.add(User("Sara1232@gmail.com","123488"))
-        arr.add(User("Ali4423@gmail.com","123455"))
+
+    private fun login() {
+        val arr: ArrayList<User> = ArrayList()
+        arr.add(User("Seeta123@gmail.com", "1234"))
+        arr.add(User("Sara1232@gmail.com", "123488"))
+        arr.add(User("Ali4423@gmail.com", "123455"))
         buttonLogin?.setOnClickListener {
-            val username =editTextUsername?.text.toString()
-            val password = editTextPassword?.text.toString()
-            val user = User(username,password)
-            for (userArry in arr){
-                if (userArry.email==user.email&&userArry.password==user.password){
-                    Toast.makeText(this,"Welcome ${user.email}",Toast.LENGTH_LONG).show()
-                }else{
-                    Toast.makeText(this,"check your data",Toast.LENGTH_LONG).show()
+            val username = editTextUsername?.text.toString() //مدخل بواسطة المستخدم (الايميل)
+            val password = editTextPassword?.text.toString() // مدخل بواسطة المستخدم (كلمة المرور)
+            val user = User(username, password)
+            for (userArray in arr) {
+                if (userArray.email == user.email && userArray.password == user.password) {
+                    // Toast.makeText(this,"Welcome${user.email}",Toast.LENGTH_LONG).show()
+                    finish()
+                    val i = Intent(this, MainActivity::class.java)
+                    i.putExtra("email", userArray.email)
+                    startActivity(i)
+                    break
+                } else {
+                    Toast.makeText(this, "check your data", Toast.LENGTH_LONG).show()
                 }
             }
         }
-        buttonLogin?.setOnClickListener {
-            if (editTextUsername?.text?.isEmpty()==true
-                || editTextPassword?.text?.isEmpty()==true
-                || checkboxView?.isChecked !=true){
-                Toast.makeText(this,"Enter your data",Toast.LENGTH_LONG).show()
-        }else{
-            Toast.makeText(this,"success",Toast.LENGTH_LONG).show()
-            }
 
+    }
+
+    private fun checkFields() {
+        buttonLogin?.setOnClickListener {
+            if (editTextUsername?.text?.isEmpty() == true
+
+               // || checkboxView?.isChecked != true
+            ) {
+                    editTextUsername?.setError("Enter your email")
+                //Toast.makeText(this, "Enter your data", Toast.LENGTH_LONG).show()
+            } else if ( editTextPassword?.text?.isEmpty() == true) {
+                editTextPassword?.error = "Enter your password"
+               // Toast.makeText(this, "success", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
+
+
+
--- app/src/main/java/com/example/mystory/MainActivity.kt
+++ app/src/main/java/com/example/mystory/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mystory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private  var textViewEmail:TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val email =intent.getStringExtra("email")//لنقل بيانات الايميل الى الواجهه التالية (ينقل الايميل 1)
+
+        connectViews()//يعمل الربط2
+        textViewEmail?.text = email//يضع الايميل في :3textview
+    }
+    private  fun connectViews(){
+        textViewEmail = findViewById(R.id.tvEmail)
     }
 }
